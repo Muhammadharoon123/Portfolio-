ccc01051ada3f11b4f150c20cca442e19791a7c9
Muhammad Haroon <ycclubfan@gmail.com>
2026-01-01 15:58:51 +0500

    Update: modified index.js constants

--- src/assets/Images/index.js
+++ src/assets/Images/index.js
@@ -2,6 +2,5 @@ import meta from "./meta.png";
 import shopify from "./shopify.png";
 import starbucks from "./starbucks.png";
 import tesla from "./tesla.png";
-import hero from "./hero.jpg"; // Check if this is .png or .jpg in your folder
 
-export { meta, shopify, starbucks, tesla, hero };
+export { meta, shopify, starbucks, tesla };
